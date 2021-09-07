75b1f73069ca3d22e6db73304f18e811d93c74bd
abey bruck <abeybruck@gmail.com>
2021-09-07 02:04:35 -0700

    fifth commit i guess

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Done by Abey Bruck</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
